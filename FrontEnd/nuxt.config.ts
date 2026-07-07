375ce1344eb992b81db34f55065421c2cea8242a
pusat <pusatuzunlar@gmail.com>
2026-07-07 11:50:09 +0300

    klm

--- FrontEnd/nuxt.config.ts
+++ FrontEnd/nuxt.config.ts
@@ -3,5 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: [ "~/assets/css/main.css"],
-
+  modules: [
+    'vuetify-nuxt-module'
+  ],
+  vuetify: {
+    moduleOptions: {
+      /* Özel ayarlar ileride buraya gelecek */
+    }
+  }
 })
