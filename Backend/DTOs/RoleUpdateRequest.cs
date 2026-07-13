490d84548a1a083ea9a6437f7ecf9428f3e71073
Kerim Altınay <lordkingrt03@gmail.com>
2026-07-13 16:38:50 +0300

    eskiler silindi

--- Backend/DTOs/RoleUpdateRequest.cs
+++ Backend/DTOs/RoleUpdateRequest.cs
@@ -7,8 +7,5 @@ public class RoleUpdateRequest
     [StringLength(50, ErrorMessage = "Rol adı en fazla 50 karakter olabilir.")]
     public string? Name { get; set; }
 
-    public bool? CanAdd { get; set; }
-    public bool? CanEdit { get; set; }
-    public bool? CanDelete { get; set; }
-    public bool? CanAccessDashboard { get; set; }
+
 }
